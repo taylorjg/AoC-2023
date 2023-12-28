b3bf96ee23f71c8e35f59d37b054135e3fb8f3e6
Jonathan Taylor <jonathan.taylor.programmer@gmail.com>
2023-12-28 09:35:58 +0000

    Tweak

--- day01/lib.mjs
+++ day01/lib.mjs
@@ -16,12 +16,13 @@ const isDigit = (ch) => /\d/.test(ch)
 
 const matchStringToDigit = (s) => isDigit(s) ? s : mapping.get(s);
 
+const words = Array.from(mapping.keys());
+const reversedWords = words.map(reverseString);
+const regexNormalWords = new RegExp(`(${[...words, "[1-9]"].join("|")})`, "g");
+const regexReversedWords = new RegExp(`(${[...reversedWords, "[1-9]"].join("|")})`, "g");
+
 export const lineToNumber = (line) => {
   const reversedLine = reverseString(line);
-  const words = Array.from(mapping.keys());
-  const reversedWords = words.map(reverseString);
-  const regexNormalWords = new RegExp(`(${[...words, "[1-9]"].join("|")})`, "g");
-  const regexReversedWords = new RegExp(`(${[...reversedWords, "[1-9]"].join("|")})`, "g");
   const matches = Array.from(line.matchAll(regexNormalWords));
   const reverseMatches = Array.from(reversedLine.matchAll(regexReversedWords));
   const firstMatchString = matches[0][0];
